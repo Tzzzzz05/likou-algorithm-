e3057b632567d9138b2e5d5e32dc28cd430c4d54
Tzzzzz05 <t1065931341@gmail.com>
2023-11-02 22:26:26 +0800

    哈希表
    Signed-off-by: Tzzzzz05 <t1065931341@gmail.com>

---
+++ 哈希表/哈希表基本理论/HashList3.cpp
@@ -0,0 +1,12 @@
+#include "HashList3.h"
+
+int main(int argc, char const *argv[]) {
+	HashTable *ht = new HashTable();
+	const char *key[] = {"a", "b", "c", "d", "f", "z"};
+	const char *value[] = {"value1", "value2", "value3", "value4", "value5", "value6"};
+	for (int i = 0; i < 6; ++i) {
+		ht->install(key[i], value[i]);
+	}
+	ht->display();
+	return 0;
+}
